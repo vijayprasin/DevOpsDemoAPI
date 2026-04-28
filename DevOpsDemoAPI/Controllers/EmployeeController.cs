1d0b91c6a3eb2250522bd372a1d6039b4f144228
vijayprasin <vijayabiprasin@gmail.com>
2026-04-28 21:51:54 +0530

    Initial commit

---
+++ DevOpsDemoAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevOpsDemoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = new List<string>
+            {
+                "Sam",
+                "John",
+                "David"
+            };
+
+            return Ok(employees);
+        }
+    }
+}
+
